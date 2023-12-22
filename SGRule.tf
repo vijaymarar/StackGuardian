f5ec61e299fa3dec81169b1655cee37fc00171e3
Vijay Kumar <47318096+vijaymarar@users.noreply.github.com>
2023-12-22 14:44:42 +0530

    Update SGRule.tf

--- SGRule.tf
+++ SGRule.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group_rule" "example" {
   type              = "ingress"
-  from_port         = 443
-  to_port           = 443
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "sg-0c1ef6bcc750ddc10"
